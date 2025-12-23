f1a8f36027782f06038e10a274efca8d6c1870ef
cashease2025-26 <cashease2025@gmail.com>
2025-12-24 01:32:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -349,6 +349,6 @@ function generatePDF() {
     doc.save("CashEase_Report.pdf");
 }
 
-window.logout() {
+window.logout=function() {
     auth.signOut().then(() => window.location.href = "login.html");
 }
